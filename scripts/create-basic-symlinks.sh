85c26817b963bb2af90110f653bc7708392cd7fe
Éanna Glavin <29385958+eglavin@users.noreply.github.com>
2025-02-17 23:13:57 +0000

    add docker config

--- scripts/create-basic-symlinks.sh
+++ scripts/create-basic-symlinks.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 ln -s $HOME/dotfiles/.bashrc $HOME/.bashrc
+ln -s $HOME/dotfiles/.docker $HOME/.docker
 ln -s $HOME/dotfiles/.gitconfig $HOME/.gitconfig
 ln -s $HOME/dotfiles/.vim $HOME/.vim
 ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
