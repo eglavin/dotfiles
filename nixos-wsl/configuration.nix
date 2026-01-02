80d73172c360b68f1d69ceb1447ce45323a81ea7
Eanna Glavin <29385958+eglavin@users.noreply.github.com>
2026-01-02 00:54:47 +0000

    add home manager

--- nixos-wsl/configuration.nix
+++ nixos-wsl/configuration.nix
@@ -1,16 +1,9 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
-# NixOS-WSL specific options are documented on the NixOS-WSL repository:
-# https://github.com/nix-community/NixOS-WSL
-
 { config, lib, pkgs, ... }:
 
 {
   imports = [
-    # include NixOS-WSL modules
     <nixos-wsl/modules>
+    <home-manager/nixos>
   ];
 
   wsl.enable = true;
@@ -34,11 +27,26 @@
     pkgs.git
   ];
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It's perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.05"; # Did you read the comment?
+  home-manager.useGlobalPkgs = true;
+  home-manager.useUserPackages = true;
+  home-manager.users.eglavin = { pkgs, ... }: {
+    # This must match your system stateVersion
+    home.stateVersion = "25.05";
+
+    # 3. Move your user-specific packages here
+    home.packages = with pkgs; [
+      htop
+      fzf
+      ripgrep
+    ];
+
+    # 4. Example: Manage Git via Home Manager
+    programs.git = {
+      enable = true;
+      userName = "Eanna Glavin";
+      userEmail = "29385958+eglavin@users.noreply.github.com";
+    };
+  };
+
+  system.stateVersion = "25.05";
 }
